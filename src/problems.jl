966c8918ecf7b7eac137a964b2f291cdcc311620
YingboMa <mayingbo5@gmail.com>
2017-07-29 19:13:30 -0400

    Add NBodyProblem function

--- src/problems.jl
+++ src/problems.jl
@@ -30,3 +30,30 @@ function LagrangianProblem(f,u0,tspan; iip = isinplace(f[2],4),callback=nothing)
   @assert length(u0) == 2
   LagrangianProblem{typeof(u0),eltype(tspan),iip,typeof(f),typeof(callback)}(f,u0,tspan,callback)
 end
+
+function NBodyProblem(V,mass_matrix,u0,v0,tspan; kwargs...)
+    @assert length(u0) == length(v0)
+    @assert isa(V(tspan[1], u0), Real)
+    NBodyProblem(Val{isdiag(mass_matrix)},V,mass_matrix,u0,v0,tspan; kwargs...)
+end
+
+function NBodyProblem(::Type{Val{true}},V,mass_matrix,u0,v0,tspan; kwargs...)
+    function acceleration!(t, x, v, dv)
+        ForwardDiff.gradient!(dv, q->V(t, q), x)
+        for i in eachindex(dv)
+            dv[i] /= -mass_matrix.diag[i]
+        end
+    end
+    SecondOrderODEProblem(acceleration!, u0, v0, tspan; kwargs...)
+end
+
+function NBodyProblem(::Type{Val{false}},V,mass_matrix,u0,v0,tspan; kwargs...)
+    factorized_M = cholfact(mass_matrix)
+    function acceleration!(t, x, v, dv)
+        ForwardDiff.gradient!(v, q->V(t, q), x)
+        A_ldiv_B!(dv, factorized_M, v)
+        scale!(dv, -1)
+    end
+    SecondOrderODEProblem(acceleration!, u0, v0, tspan; kwargs...)
+end
+
