7df6baaa7520b8ec9d3c66b4a57f4b411e1f5817
YingboMa <mayingbo5@gmail.com>
2017-07-31 18:17:25 -0400

    Add HamiltonianProblem

--- src/DiffEqPhysics.jl
+++ src/DiffEqPhysics.jl
@@ -5,7 +5,7 @@ module DiffEqPhysics
 using Reexport
 @reexport using DiffEqBase, OrdinaryDiffEq, ForwardDiff
 
-include("problem.jl")
+include("nbody.jl")
 include("hamiltonian.jl")
 
 export HamiltonianProblem, LagrangianProblem, NBodyProblem
--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,29 +1,25 @@
 using OrdinaryDiffEq, ForwardDiff
 
-function HamiltonianProblem(
-  H::F,q0::Real,p0::Real,tspan::Tuple{T,T}; kwargs...) where T<:Real
+function HamiltonianProblem(H,q0::Real,p0::Real,tspan::Tuple{T,T};kwargs...) where T<:Real
+    @inline function dq(t,x,v,dx)
+        dx[1] = ForwardDiff.derivative(v->H(x[1], v), v[1])
+    end
+    @inline function dp(t,x,v,dv)
+        dv[1] = ForwardDiff.derivative(x->-H(x, v[1]), x[1])
+    end
 
-  D = length(q0)
-  Ham = (x) -> H(x[1], x[2])
-  function EOM(Ham)
-    grad!(result, x) = ForwardDiff.gradient!(result, Ham, x::AbstractArray)
-    #magic
-  return dHdq, dHdp
-
-  dHdq, dHdp = EOM(HAM)
-  return ODEProblem((dHdp,-dHdq), (q0,p0), tspan; kwargs...)
+    return ODEProblem((dq,dp), ([q0],[p0]), tspan; kwargs...)
 end
 
-function HamiltonianProblem(
-  H::F,q0::AbstractVector,p0::AbstractVector,tspan::Tuple{T,T}; kwargs...) where T<:Real
+function HamiltonianProblem(H,q0::AbstractVector,p0::AbstractVector,tspan::Tuple{T,T};kwargs...) where T<:Real
+    @assert length(q0) == length(p0)
+    @inline function dq(t,x,v,dx)
+        ForwardDiff.gradient!(dx, v-> H(x, v), v)
+    end
+    @inline function dp(t,x,v,dv)
+        ForwardDiff.gradient!(dv, x->-H(x, v), x)
+    end
 
-  D = length(q0)
-  Ham = (x) -> H(x[1:D], x[D+1:end])
-  function EOM(Ham, D)
-    grad!(result, x) = ForwardDiff.gradient!(result, Ham, x::AbstractArray)
-    #magic
-  return dHdq, dHdp
-
-  dHdq, dHdp = EOM(HAM)
-  return ODEProblem((dHdp,-dHdq), (q0,p0), tspan; kwargs...)
+    return ODEProblem((dq,dp), (q0,p0), tspan; kwargs...)
 end
+
