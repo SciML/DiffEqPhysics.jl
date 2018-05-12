233733d3ece3f70cf808e397e1f092f9d1c7adfc
Mikhail Vaganov <arzamas-17@mail.ru>
2018-05-12 20:02:14 +0500

    Added implementation for the gravitational n-body simmulations

--- src/DiffEqPhysics.jl
+++ src/DiffEqPhysics.jl
@@ -9,7 +9,13 @@ using ForwardDiff, StaticArrays, RecipesBase
 include("nbody.jl")
 include("hamiltonian.jl")
 include("plot.jl")
+include("nbody_gravitational")
+include("animation_gravitational")
 
 export HamiltonianProblem, LagrangianProblem, NBodyProblem, plot_orbits
+
+export NBodyGravProblem, MassBody, GravImagingData, solve, 
+	   plot_xy_trailing, plot_xy_scattering, plot_xy
+
 
 end # module
