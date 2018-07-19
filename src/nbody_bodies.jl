3f657af4402c9a73212d8ccc53d1253db8a8a627
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-18 17:41:52 -0700

    get it precompiling

--- src/nbody_bodies.jl
+++ src/nbody_bodies.jl
@@ -1,9 +1,9 @@
 # This is an attempt to include the required for n-body simulations fields into structures
-# Indeed, tools of ConcreteAbstractions.jl seem to be more suitable for the fields inhereting 
-@def position_velocity_mass begin
-    r::SVector{3,cType} 
+# Indeed, tools of ConcreteAbstractions.jl seem to be more suitable for the fields inhereting
+DiffEqBase.@def position_velocity_mass begin
+    r::SVector{3,cType}
     v::SVector{3,cType}
-    m::mType           
+    m::mType
 end
 
 abstract type Body
@@ -15,10 +15,10 @@ end
 
 struct ChargedParticle{cType <: Real,mType <: Real,qType <: Real} <: Body
     @position_velocity_mass
-    q::qType     
+    q::qType
 end
 
 struct MagneticParticle{cType <: Real,mType <: Real,mmType <: Real} <: Body
     @position_velocity_mass
-    mm::SVector{3,mmType}   
+    mm::SVector{3,mmType}
 end
