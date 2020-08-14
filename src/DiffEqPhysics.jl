3e8f9e9e99d23c9e1bc16421b8b7582fdf6358a5
St Z <abczst@outlook.com>
2020-08-14 22:32:02 +0800

    remove all about NBodyProblem

--- src/DiffEqPhysics.jl
+++ src/DiffEqPhysics.jl
@@ -5,21 +5,9 @@ using Reexport
 using ForwardDiff, StaticArrays, RecipesBase
 using Random, Printf, LinearAlgebra
 
-include("nbody_problem.jl")
 include("hamiltonian.jl")
 include("plot.jl")
 
-export HamiltonianProblem, NBodyProblem, plot_orbits
-
-export NBodySimulation
-export MassBody, ChargedParticle, MagneticParticle
-export PotentialParameters, LennardJonesParameters, GravitationalParameters,
-       ElectrostaticParameters, MagnetostaticParameters, SPCFwParameters
-export PotentialNBodySystem, ChargedParticles, GravitationalSystem, WaterSPCFw
-export PeriodicBoundaryConditions, CubicPeriodicBoundaryConditions, InfiniteBox
-export AndersenThermostat, BerendsenThermostat, NoseHooverThermostat, LangevinThermostat
-export run_simulation, get_position, get_velocity, get_masses, temperature,
-       initial_energy, kinetic_energy, potential_energy, total_energy, rdf, msd,
-       generate_bodies_in_cell_nodes, run_simulation_sde
+export HamiltonianProblem, plot_orbits
 
 end # module
