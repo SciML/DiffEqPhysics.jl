e8fbd2822154d7a855995c66f44aa3d59673eb96
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-20 10:43:39 -0400

    Add representative PrecompileTools workload
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- src/DiffEqPhysics.jl
+++ src/DiffEqPhysics.jl
@@ -1,5 +1,6 @@
 module DiffEqPhysics
 
+using PrecompileTools: @compile_workload, @setup_workload
 using RecursiveArrayTools: ArrayPartition
 using DifferentiationInterface: AutoForwardDiff, derivative, gradient, gradient!
 import ForwardDiff
@@ -13,5 +14,16 @@ include("hamiltonian.jl")
 include("plot.jl")
 
 export HamiltonianProblem, orbitplot, plot_orbits
+
+@setup_workload begin
+    @compile_workload begin
+        H(p, q, _, _) = p^2 / 2 + q^2 / 2
+        HamiltonianProblem(H, 1.0, 0.0, (0.0, 1.0))
+
+        dp(p, q, _, _) = -q
+        dq(p, q, _, _) = p
+        HamiltonianProblem((dp, dq), 1.0, 0.0, (0.0, 1.0))
+    end
+end
 
 end # module
